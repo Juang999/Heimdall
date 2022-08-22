3e1d8e528776c5e40979cd684f8c80b95e3be7c2
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-08-22 10:06:16 +0700

    update models
    update models

--- app/Models/SoDDetail.php
+++ app/Models/SoDDetail.php
@@ -12,6 +12,10 @@ class SoDDetail extends Model
 
     protected $primaryKey = 'sod_oid';
 
+    protected $fillable = ['sod_upd_by', 'sod_upd_date', 'sod_qty_checked'];
+
+    public $timestamps = false;
+
     public function SoMaster()
     {
         return $this->belongsTo(SoMaster::class, 'so_oid');
