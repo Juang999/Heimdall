3cc78b8b494f7ff20d2e20db50949d6343c8a03d
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-08-24 09:14:11 +0700

    group's model
    model for manage tconfgroup

---
+++ app/Models/TConfGroup.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TConfGroup extends Model
+{
+    protected $table = 'public.tconfgroup';
+
+    protected $primaryKey = 'groupid';
+}
