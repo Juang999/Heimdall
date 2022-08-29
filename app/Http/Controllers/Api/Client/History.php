df18dff479fa83191249fcd936d411f1a22b1396
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-08-30 05:58:24 +0700

    history
    make history

---
+++ app/Http/Controllers/Api/Client/History.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\Client;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\SoDDetail;
+
+class History extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke()
+    {
+        try {
+            // 
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::middleware('jwt.verify')->group( function () {
 
     Route::prefix('user')->group( function () {
         Route::get('profile', 'Api\Client\Profile');
+        // Route::get('history', 'Api\Client\')
     });
 
     Route::get('/pt/{pt_code}', 'Api\Client\CheckProduct');
