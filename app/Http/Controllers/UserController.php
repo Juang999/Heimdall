1987080abdd4c415adc53c7c246956c230b22c40
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-09-02 09:19:03 +0700

    profile feature
    add group or role in profile featiure

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use JWTAuth;
 use App\User;
+use App\Models\TConfGroup;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -15,100 +16,38 @@ class UserController extends Controller
 {
     public function login(Request $request)
     {
-        $user = User::where([
-            'usernama' => $request->usernama,
-            'password' => $request->password
-        ])->first();
-
-        if (!$user) {
-            return response()->json([
-                'status' => 'rejected!',
-                'message' => 'user not found!'
-            ], 300);
-        }
-
         try {
-            if (! $token = JWTAuth::fromUser($user)) {
-                return response()->json(['error' => 'invalid_credentials'], 400);
+            $user = User::where([
+                'usernama' => $request->usernama,
+                'password' => $request->password
+            ])->first();
+
+            if (!$user) {
+                return response()->json([
+                    'status' => 'rejected!',
+                    'message' => 'username or password incorrect'
+                ], 300);
             }
-        } catch (JWTException $e) {
-            return response()->json(['error' => 'could_not_create_token'], 500);
+
+            $token = JWTAuth::fromUser($user);
+
+            return response()->json([
+                'token' => $token
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'failed',
+                'pesan' => 'gagal untuk masuk',
+                'galat' => $th->getMessage()
+            ], 400);
         }
-
-        return response()->json(compact('token'));
     }
-
-    // public function register(Request $request)
-    // {
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|email|max:255|unique:users',
-    //         'password' => 'required|string|min:6|confirmed',
-    //     ]);
-
-    //     if($validator->fails()){
-    //         return response()->json($validator->errors()->toJson(), 400);
-    //     }
-
-    //     $user = User::create([
-    //         'name' => $request->get('name'),
-    //         'email' => $request->get('email'),
-    //         'password' => Hash::make($request->get('password')),
-    //     ]);
-
-    //     $token = JWTAuth::fromUser($user);
-
-    //     return response()->json(compact('user','token'),201);
-    // }
-
-    // public function getAuthenticatedUser()
-    // {
-    //     try {
-
-    //         if (! $user = JWTAuth::parseToken()->authenticate()) {
-    //             return response()->json(['user_not_found'], 404);
-    //         }
-
-    //     } catch (PHPOpenSourceSaver\JWTAuth\Exceptions\TokenExpiredException $e) {
-
-    //         return response()->json(['token_expired'], $e->getStatusCode());
-
-    //     } catch (PHPOpenSourceSaver\JWTAuth\Exceptions\TokenInvalidException $e) {
-
-    //         return response()->json(['token_invalid'], $e->getStatusCode());
-
-    //     } catch (PHPOpenSourceSaver\JWTAuth\Exceptions\JWTException $e) {
-
-    //         return response()->json(['token_absent'], $e->getStatusCode());
-
-    //     }
-
-    //     return response()->json(compact('user'));
-    // }
-
-    // public function getUser()
-    // {
-    //     try {
-    //         $user = User::get();
-
-    //         return response()->json([
-    //             'status' => 'success',
-    //             'message' => 'success to get data',
-    //             'data' => $user
-    //         ], 200);
-    //     } catch (\Throwable $th) {
-    //         return response()->json([
-    //             'status' => 'failed',
-    //             'message' => 'failed to get data',
-    //             'error' => $th->getMessage()
-    //         ], 200);
-    //     }
-    // }
 
     public function profile()
     {
         try {
             $user = Auth::user();
+            $user->group = TConfGroup::where('groupid', $user->groupid)->get();
 
             return response()->json([
                 'status' => 'success',
