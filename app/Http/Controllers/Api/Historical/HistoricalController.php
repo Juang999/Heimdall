8047a8a8ed3bad66f030769485830a02feafd4cb
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-09-12 10:20:52 +0700

    new controllers
    controllers for inventory requests

---
+++ app/Http/Controllers/Api/Historical/HistoricalController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\Historical;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HistoricalController extends Controller
+{
+    public function invc()
+    {
+        try {
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+}
